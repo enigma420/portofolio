5b976f71d404b38aeccbcb2cda9b878e31fa9a91
Enigma <enigma3420@gmail.com>
2020-02-14 20:38:55 +0100

    next push

---
+++ src/components/TechnologicalStack.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import TechStackIconTable from "./TechStackIconTable";
+
+
+class TechnologicalStack extends Component {
+    render() {
+        return (
+            <div >
+               <TechStackIconTable/>
+            </div>
+        );
+    }
+}
+
+export default TechnologicalStack;
